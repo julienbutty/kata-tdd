629734914f946d7aee8c7ab47c017051001a85ff
Julien Butty <julienbutty@gmail.com>
2023-09-17 11:27:28 +0200

    feature: FizzBuzz Kata stage 2

--- src/FizzBuzz/KataFizzBuzz.php
+++ src/FizzBuzz/KataFizzBuzz.php
@@ -3,8 +3,39 @@
 namespace App\FizzBuzz;
 class KataFizzBuzz
 {
-    public function returnAction(): int
+    public function formatInteger(int $int): int|string
     {
-        return 2;
+        $prefix = '';
+        // we check the stage 2 prefixes
+        if (str_contains((string)$int, 3)) {
+            $prefix = 'Fizz';
+        } if (str_contains((string) $int, 5)) {
+            $prefix = 'Buzz';
+        }
+        // we check the stage 1 modulo
+       $response =  match (true) {
+            $int === 0 => 0,
+            $int % 15 === 0 => 'FizzBuzz',
+            $int % 3 === 0 => 'Fizz',
+            $int % 5 === 0 => 'Buzz',
+            default => empty($prefix) ? $int : ''
+        };
+
+        return $prefix . $response;
+    }
+
+    public function processIntegersInRange(int $intStart, int $intEnd): string
+    {
+        $response = '';
+        if ($intStart === $intEnd) {
+            return "The interval can't be identical";
+        }
+        if ($intStart > $intEnd) {
+            return "The first number must be less than the last";
+        }
+        for ($i = $intStart; $i <= $intEnd; $i++) {
+           $response .= "{$this->formatInteger($i)}\n";
+        }
+        return nl2br($response);
     }
 }
--- tests/FizzBuzz/KataTest.php
+++ tests/FizzBuzz/KataTest.php
@@ -7,10 +7,60 @@ use PHPUnit\Framework\TestCase;
 
 class KataTest extends TestCase
 {
-    public function testReturnAction()
-    {
-        $kata = new KataFizzBuzz();
+    private KataFizzBuzz $kata;
 
-        $this->assertEquals(2, $kata->returnAction());
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->kata = new KataFizzBuzz();
+    }
+
+    /**
+     * @dataProvider provideDataToFormat
+     */
+    public function testFormatInteger($int, $expect)
+    {
+        $this->assertEquals($expect, $this->kata->formatInteger($int));
+    }
+
+    /**
+     * @dataProvider provideDataToProcessRange
+     */
+    public function testProcessIntegersInRangeFrom1to3($startInt, $endInt, $expected)
+    {
+        $this->assertEquals($expected, $this->kata->processIntegersInRange($startInt, $endInt));
+    }
+
+    public static function provideDataToFormat(): array
+    {
+        return [
+            // should return integer
+            [1, 1],
+            [2,2],
+            // should return Fizz
+            [3, 'FizzFizz'],
+            [18, 'Fizz'],
+            // should return Buzz
+            [5, 'BuzzBuzz'],
+            [10, 'Buzz'],
+            // should return FizzBuzz
+            [15, 'BuzzFizzBuzz'],
+            [45, 'BuzzFizzBuzz'],
+            // boundaries
+            [0, 0],
+        ];
+    }
+
+    public static function provideDataToProcessRange(): array
+    {
+        return [
+            // basic process
+            [ 1, 2, nl2br("1\n2\n") ],
+            [ 1, 3, nl2br("1\n2\nFizzFizz\n") ],
+            [ 1, 15, nl2br("1\n2\nFizzFizz\n4\nBuzzBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\nFizz\n14\nBuzzFizzBuzz\n")],
+            // boundaries process
+            [ 0, 0, "The interval can't be identical"],
+            [3,2, "The first number must be less than the last"]
+        ];
     }
 }
